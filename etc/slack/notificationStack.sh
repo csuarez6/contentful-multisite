2e51bbe965e30fa8e43622b93196c6ce69f91d45
Brandon Stiven Sanchez Mojica <bsanchez@aplyca.com>
2022-12-02 00:07:55 +0000

    fix(notification): updated slack notification payload
    Notificación de slack

--- etc/slack/notificationStack.sh
+++ etc/slack/notificationStack.sh
@@ -60,18 +60,18 @@ deleteFile(){
         return $?
 }
 
-PAYLOAD=$(cat << _EOF_ > doc/slack/payload.json
+PAYLOAD=$(cat << _EOF_ > etc/slack/payload.json
 {
 	"text": "*GitLab CI/CD Job*",
 	"username":"${USERNAME}",
 	"icon_url": "https://about.gitlab.com/images/press/press-kit-icon.svg",
 	"attachments": [{
-		"color": "#37376E",
+		"color": "#F6CC5C",
 		"fields": [
-			{ "title": "Notification", "value": "The Gitlab Job <${CI_JOB_URL} |${CI_JOB_NAME}> was started by ${GITLAB_USER_NAME} in the project <${CI_PROJECT_URL} |${CI_PROJECT_NAME}>", "short": false },
+			{ "title": "Notificación", "value": "El job de gitlab <${CI_JOB_URL} |${CI_JOB_NAME}> fue iniciado por ${GITLAB_USER_NAME} en el proyecto <${CI_PROJECT_URL} |${CI_PROJECT_NAME}>", "short": false },
 			__COMMITTAG__			
-			{ "title": "Link to Pipeline", "value": "Please, follow the pipeline status in: ${CI_PIPELINE_URL}", "short": false},
-			{ "title": "Job finished at", "value": "${TIMESTAMP}", "short": false}
+			{ "title": "Link al pipeline", "value": "Puedes seguir el stado del pipeline en la siguiente URL: ${CI_PIPELINE_URL}", "short": false},
+			{ "title": "Job finalizado", "value": "${TIMESTAMP}", "short": false}
 		]
 	}]
 }
@@ -80,24 +80,24 @@ _EOF_
 
 if [ -z ${CI_COMMIT_TAG} ]
 then
-	sed -i '/__COMMITTAG__/d' doc/slack/payload.json
+	sed -i '/__COMMITTAG__/d' etc/slack/payload.json
 else
-	TAG_TEXT="{ 'title': 'Tag Trigger', 'value': 'It was created the tag *${CI_COMMIT_TAG}*', 'short': false },"
-	sed -i "s/__COMMITTAG__/${TAG_TEXT}/g" doc/slack/payload.json
+	TAG_TEXT="{ 'title': 'Tag Trigger', 'value': 'Se ha creado el tag *${CI_COMMIT_TAG}*', 'short': false },"
+	sed -i "s/__COMMITTAG__/${TAG_TEXT}/g" etc/slack/payload.json
 fi
 
-logit INFO "Webhook payload: "$(cat doc/slack/payload.json)
+logit INFO "Webhook payload: "$(cat etc/slack/payload.json)
 
-curl -s -o /dev/null -X POST ${SLACK_WEBHOOK} -d @doc/slack/payload.json
+curl -s -o /dev/null -X POST ${SLACK_WEBHOOK} -d @etc/slack/payload.json
 
 if [ $? -eq 0 ]
 then
 	logit INFO "Notification sent to Slack"
 else
-	deleteFile doc/slack/payload.json
+	deleteFile etc/slack/payload.json
 	logit ERROR "There was an error to send the notification to Slack"
 fi
 
-deleteFile doc/slack/payload.json
+deleteFile etc/slack/payload.json
 
 exit 0
