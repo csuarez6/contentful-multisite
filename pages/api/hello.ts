1a1ecb401dee5760454e831327d75a94368d3560
Brandon Stiven Sanchez Mojica <bsanchez@aplyca.com>
2023-01-23 16:09:18 +0000

    fix(emailjs): Test email smtp AWS
    Implement product filters

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -8,6 +8,7 @@ type Data = {
   name?: string | string[],
   error?: any,
   message?: any,
+  connData?: any,
 };
 
 export default function handler(
@@ -21,23 +22,35 @@ export default function handler(
     return;
   }
 
+  console.log(typeof req.query.user);
+  const user = req.query.user ? (typeof req.query.user !== 'string' ? req.query.user[0] : req.query.user) : process.env.SMTP_USER;
+  const pass = req.query.pass ? (typeof req.query.pass !== 'string' ? req.query.pass[0] : req.query.pass) : process.env.SMTP_PASSWORD;
+  const port = req.query.port ? (typeof req.query.port !== 'string' ? req.query.port[0] : req.query.port) : process.env.SMTP_PORT;
+  const host = req.query.host ? (typeof req.query.host !== 'string' ? req.query.host[0] : req.query.host) : process.env.SMTP_HOST;
+  const ssl = req.query.ssl ? (typeof req.query.ssl !== 'string' ? req.query.ssl[0] : req.query.ssl) : 0;
+  const to = req.query.to ? (typeof req.query.to !== 'string' ? req.query.to[0] : req.query.to) : 'bsanchez@aplyca.com';
+
+  const connData = {
+    host, port, user, pass, ssl: (ssl == '1' ? true : false), to
+  };
+
   const client = new SMTPClient({
-    user: process.env.SMTP_USER,
-    password: process.env.SMTP_PASSWORD,
-    host: process.env.SMTP_HOST,
-    ssl: false,
-    port: parseInt(process.env.SMTP_PORT),
+    user,
+    password: pass,
+    host,
+    ssl: ssl == '1' ? true : false,
+    port: parseInt(port),
   });
 
   client.send(
     {
       text: 'i hope this works',
       from: 'Vanti <no-reply@grupovanti.com>',
-      to: 'Brandon <bsanchez@aplyca.com>',
+      to: `Brandon <${to}>`,
       subject: 'testing emailjs',
     },
     (err, message) => {
-      res.status(200).json({ message, error: err });
+      res.status(200).json({ connData, message, error: err });
     }
   );
 }
