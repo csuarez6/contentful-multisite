8f99bd119e76eca724ace00b6d81a5edb174dfff
Andres Garcia <agarcia@aplyca.com>
2023-01-24 20:01:06 +0000

    fix(entry-content): increase number of recursions
    fix(entry-content): increase number of recursions

--- src/lib/services/entry-content.service.ts
+++ src/lib/services/entry-content.service.ts
@@ -34,7 +34,7 @@ type DefaultBlockInfo = {
   }
 };
 
-export const MAX_DEEPTH_RECURSION = 12;
+export const MAX_DEEPTH_RECURSION = 14;
 
 const getEntryContent = async (blockInfo: DefaultBlockInfo, preview = false, recursive = true, actualDeepth = 1) => {
   if (!blockInfo || !CONTENTFUL_QUERY_MAPS[blockInfo.__typename]) {
