8e9de57c4219c4869c7e74df568111ee3f65f841
Andres Garcia <agarcia@aplyca.com>
2023-01-25 21:19:10 +0000

    fix(error): implement contentful header and footer in  error page
    fix(error): implement contentful header and footer in  error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -2,7 +2,8 @@ import NextErrorComponent from "next/error";
 
 import * as Sentry from "@sentry/nextjs";
 
-import { mockPageLayoutProps } from "@/components/layouts/page-layout/PageLayout.mocks";
+import { DEFAULT_FOOTER_ID, DEFAULT_HEADER_ID } from "@/constants/contentful-ids.constants";
+import { getMenu } from "@/lib/services/menu-content.service";
 
 const CustomErrorComponent = (props: any) => {
   return <NextErrorComponent statusCode={props.statusCode} />;
@@ -13,14 +14,17 @@ CustomErrorComponent.getInitialProps = async (context: any) => {
     await Sentry.captureUnderscoreErrorException(context);
   }
 
+  const headerInfo = await getMenu(DEFAULT_HEADER_ID, context.preview ?? false);
+  const footerInfo = await getMenu(DEFAULT_FOOTER_ID, context.preview ?? false, 2);
+
   // This will contain the status code of the response
   const errors = NextErrorComponent.getInitialProps(context);
   return {
     ...errors,
     layout: {
-      name: mockPageLayoutProps.data.name,
-      footerInfo: mockPageLayoutProps.data.layout.footerInfo,
-      headerInfo: mockPageLayoutProps.data.layout.headerInfo,
+      name: 'Error 404',
+      footerInfo,
+      headerInfo,
     },
   };
 };
