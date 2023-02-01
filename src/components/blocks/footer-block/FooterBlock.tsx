a89a703d0fcddc9408ee1280d03b2caefd2a3ba7
Michael Cermeño <mcermeno@aplyca.com>
2023-02-01 22:26:23 +0000

    fix(footer): add responsive footer for mobile
    fix(footer): add responsive footer for mobile

--- src/components/blocks/footer-block/FooterBlock.tsx
+++ src/components/blocks/footer-block/FooterBlock.tsx
@@ -5,6 +5,8 @@ import { INavigation } from "@/lib/interfaces/menu-cf.interface";
 
 import CustomLink from "@/components/atoms/custom-link/CustomLink";
 import Icon from "@/components/atoms/icon/Icon";
+import { Disclosure } from "@headlessui/react";
+
 
 const FooterBlock: React.FC<INavigation> = ({
   name,
@@ -24,12 +26,14 @@ const FooterBlock: React.FC<INavigation> = ({
         {name ?? "Footer"}
       </h2>
       <div className="xl:container mx-auto">
-        <div className="py-12 px-2 sm:px-4 lg:px-8 2xl:px-[46px] lg:pt-10 lg:pb-[62px]">
-          <div className="xl:flex xl:gap-32 pb-10">
-            <div className="xl:max-w-xs pt-0.5">
+        <div className="pb-[25px] pt-6 px-2 sm:px-4 lg:px-8 2xl:px-[46px] lg:pt-10 lg:pb-[62px]">
+          <div className="xl:flex-row md:gap-x-32 pb-3 items-center flex flex-col md:items-start">
+
+            {/* Desktop  Navigation*/}
+            <div className="xl:!max-w-xs pt-0.5 hidden md:block">
               <p className="title is-1 text-white">{promoTitle}</p>
             </div>
-            <div className="pt-12 grid xl:grow xl:pt-0">
+            <div className="hidden pt-12 md:grid md:grow xl:pt-0 md:w-full">
               <div className="grid sm:grid-cols-2 lg:grid-cols-4 md:gap-8">
                 {mainNavCollection?.items?.map((menuItem) => (
                   <div key={menuItem.sys.id}>
@@ -54,8 +58,53 @@ const FooterBlock: React.FC<INavigation> = ({
                 ))}
               </div>
             </div>
+
+            {/* Mobile navigation */}
+            <div className="xl:max-w-xs pt-0.5 max-w-[225px] md:max-w-none md:hidden">
+              <p className="text-white  title is-1 !text-lg text-center !leading-5">{promoTitle}</p>
+            </div>
+            <div className="md:hidden border-b-[0.5px] border-white mt-[33px] w-[105%]">
+              {mainNavCollection?.items?.map((menuItem) => (
+                <Disclosure as="div" className='border-t-[0.5px] border-white' key={menuItem.sys.id}>
+                  {({ open }) => (
+                    <div className="px-[10px]">
+                      <dt className="text-lg">
+                        <Disclosure.Button
+                          className={`flex w-full items-center justify-between pl-2 text-left text-gray-400 pt-[8px] ${!open ? "mb-[8px]" : ""}`}>
+                          <div className="flex">
+                            <h3 className="text-white text-sm">
+                              {menuItem.promoTitle ?? menuItem.name}
+                            </h3>
+                          </div>
+                          <span className={`flow-root shrink-0 w-7 h-7 mr-2`}>
+                            <Icon icon={open ? 'substract' : 'add'} className="mx-auto w-full h-full text-white" />
+                          </span>
+                        </Disclosure.Button>
+                      </dt>
+                      <Disclosure.Panel as="dd" className={`pl-2 pr-12 ${open ? "mb-[10px]" : ""}`}>
+                        {menuItem?.mainNavCollection?.items?.length > 0 && (
+                          <ul role="list" className="">
+                            {menuItem.mainNavCollection.items.map((listItem) => (
+                              <li key={listItem.sys.id}>
+                                <CustomLink
+                                  content={listItem}
+                                  className="text-sm text-white hover:underline"
+                                >
+                                  {listItem.promoTitle ?? listItem.name}
+                                </CustomLink>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </Disclosure.Panel>
+                    </div>
+                  )}
+                </Disclosure>
+              ))}
+            </div>
+
           </div>
-          <ul className="flex flex-wrap justify-center pt-4 pb-2 gap-16">
+          <ul className="flex flex-wrap justify-center pt-4 pb-2 gap-2 md:gap-16">
             {secondaryNavCollection?.items?.map((item) => (
               <li key={item.sys.id}>
                 <CustomLink
@@ -76,13 +125,13 @@ const FooterBlock: React.FC<INavigation> = ({
               </li>
             ))}
           </ul>
-          <hr className="min-w-[100vw] -mx-[50vw] border-t-[0.5px] border-neutral-70" />
-          <div className="pt-10 flex flex-col sm:flex-row justify-between gap-16">
-            <div className="text-size-p2 text-white">
+          <hr className="min-w-[100vw] -mx-[50vw] border-t-[0.5px] border-neutral-70 hidden md:block" />
+          <div className="pt-[17px] md:pt-10 flex flex-col sm:flex-row justify-between gap-[30px] items-center">
+            <div className="text-size-p2 text-white text-center md:text-start leading-none">
               {documentToReactComponents(secondaryText?.json)}
             </div>
             {promoImage && (
-              <div className="w-[311px] mt-1 shrink-0 -mr-[7px]">
+              <div className="w-[248px] md:w-[311px] mt-1 shrink-0 -mr-[7px]">
                 <figure className="relative w-100">
                   <Image
                     src={promoImage.url}
