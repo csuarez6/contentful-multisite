b4ba8d3da5d7297922b69e5d1677da022e8a2023
Duvan Navarro <dnavarro@aplyca.com>
2023-02-04 00:33:31 +0000

    fix(fix-middlewareRedirect): fix Middleware
    fix(fix-middlewareRedirect): fix Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,37 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import { getSession } from 'next-auth/react';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const requestForNextAuth = {
-    headers: {
-      cookie: req.headers.get('cookie'),
-      IncomingMessage: '',
-    },
-  };
-
-  // const session = await getSession({ req: requestForNextAuth });
-
-  // if (session) {
-  //   // validate your session here
-  //   console.log("********** Middleware Response: User Session Actived");
-  //   return NextResponse.next();
-  // } else {
-  //   // the user is not logged in, redirect to the sign-in page
-  //   console.log("********** Middleware Response: No User Session");
-  //   const requestedPage = req.nextUrl.pathname;
-  //   const url = req.nextUrl.clone();
-  //   url.pathname = `/acceso`;
-  //   url.search = `p=${requestedPage}`;
-  //   return NextResponse.redirect(url);
+import { getToken } from "next-auth/jwt";
+// paths that require authentication or authorization
+// const requireAuth: string[] = ["/registro"];
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: 'secret',
+  });
+  console.log("********** middleware: ", token);
+  if (token) {
+    // validate your session here
+    console.log("********** Middleware Response: User Session Actived");
+    return NextResponse.next();
+  } else {
+    // the user is not logged in, redirect to the sign-in page
+    console.log("********** Middleware Response: No User Session");
+    const requestedPage = request.nextUrl.pathname;
+    const url = request.nextUrl.clone();
+    url.pathname = `/acceso`;
+    url.search = `p=${requestedPage}`;
+    return NextResponse.redirect(url);
+  }
+  // if (requireAuth.some((path) => pathname.startsWith(path))) {
+  //   //check role
+  //   if (token.role !== "admin") {
+  //     const url = new URL(`/403`, request.url);
+  //     return NextResponse.rewrite(url);
+  //   }
   // }
-};
+}
 
-// See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: ['/checkout/verify', '/registro']
-// };
+// // See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/checkout/verify']
+};
