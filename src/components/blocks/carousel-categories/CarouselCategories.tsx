583f0c34a0a5cc41173345196daffc699bf476e8
Michael Cermeño <mcermeno@aplyca.com>
2023-02-13 15:54:07 +0000

    fix(carousel-categories): add swiper breakpoints to carousel
    fix(carousel-categories): add swiper breakpoints to carousel

--- src/components/blocks/carousel-categories/CarouselCategories.tsx
+++ src/components/blocks/carousel-categories/CarouselCategories.tsx
@@ -35,110 +35,129 @@ const CarouselCategoriesBlock: React.FC<
   sysId,
   queryParamName = "categoria",
 }) => {
-  const uui = uuid();
-  const allowTouchMove = view?.isSlider ?? true;
-  const carouselAlignTitleClass = () => {
-    switch (view.alignTitle) {
-      case "Left":
-        return "md:w-[331px] md:max-w-[25%] md:text-left md:shrink-0";
-      case "Top":
-        return "text-center mb-3";
-      default:
-        return "mb-3";
-    }
-  };
+    const uui = uuid();
+    const allowTouchMove = view?.isSlider ?? true;
+    const carouselAlignTitleClass = () => {
+      switch (view.alignTitle) {
+        case "Left":
+          return "md:w-[331px] md:max-w-[25%] md:text-left md:shrink-0";
+        case "Top":
+          return "text-center mb-3";
+        default:
+          return "mb-3";
+      }
+    };
 
-  return (
-    <section
-      id={blockId ? blockId : sysId}
-      className={classNames(
-        "section gap-9 flex flex-col",
-        view?.alignTitle === "Left" && "md:flex-row md:gap-0"
-      )}
-    >
-      {(title || description) && (
-        <div
-          className={classNames(
-            carouselAlignTitleClass()
-          )}
-        >
-          {title && <h2 className="text-blue-dark text-4xl">{title}</h2>}
-          {description && (
-            <div className="text-blue-dark">
-              {documentToReactComponents(description.json)}
-            </div>
-          )}
-        </div>
-      )}
-      {featuredContentsCollection?.items?.length > 0 && (
-        <div
-          className={classNames(
-            "flex flex-nowrap relative grow w-full",
-            view?.alignTitle === "Left" && "md:max-w-[75%] md:pl-9"
-          )}
-        >
-          {allowTouchMove && (
-            <div className="flex justify-center items-center">
-              <div
-                className={`prevSlide${uui} bg-blue-dark-90 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center`}
-              >
-                <Icon {...iconLeft} />
-              </div>
-            </div>
-          )}
-          <Swiper
-            allowTouchMove={allowTouchMove}
-            slidesPerView={view?.columnsSize ?? 5}
-            spaceBetween={0}
-            slidesPerGroup={1}
-            loop={true}
-            loopFillGroupWithBlank={true}
-            modules={[Pagination, Navigation]}
-            navigation={{
-              nextEl: `.nextSlide${uui}`,
-              prevEl: `.prevSlide${uui}`,
-            }}
-            className="relative w-full"
+    return (
+      <section
+        id={blockId ? blockId : sysId}
+        className={classNames(
+          "section gap-9 flex flex-col",
+          view?.alignTitle === "Left" && "md:flex-row md:gap-0"
+        )}
+      >
+        {(title || description) && (
+          <div
+            className={classNames(
+              carouselAlignTitleClass()
+            )}
           >
-            {featuredContentsCollection &&
-              featuredContentsCollection?.items?.map((content) => (
-                <SwiperSlide key={content.sys.id}>
+            {title && <h2 className="text-blue-dark text-4xl">{title}</h2>}
+            {description && (
+              <div className="text-blue-dark">
+                {documentToReactComponents(description.json)}
+              </div>
+            )}
+          </div>
+        )}
+        {featuredContentsCollection?.items?.length > 0 && (
+          <div
+            className={classNames(
+              "flex flex-nowrap relative grow w-full",
+              view?.alignTitle === "Left" && "md:max-w-[75%] md:pl-9"
+            )}
+          >
+            {allowTouchMove && (
+              <div className="flex justify-center items-center">
+                <div
+                  className={`prevSlide${uui} bg-blue-dark-90 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center`}
+                >
+                  <Icon {...iconLeft} />
+                </div>
+              </div>
+            )}
+            <Swiper
+              allowTouchMove={allowTouchMove}
+              slidesPerView={1}
+              breakpoints={{
+                375: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                500: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 30,
+                },
+                960: {
+                  slidesPerView: 5,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: view?.columnsSize ?? 5,
+                  spaceBetween: 40,
+                }
+              }}
+              loop={true}
+              loopFillGroupWithBlank={true}
+              modules={[Pagination, Navigation]}
+              navigation={{
+                nextEl: `.nextSlide${uui}`,
+                prevEl: `.prevSlide${uui}`,
+              }}
+              className="relative w-full"
+            >
+              {featuredContentsCollection?.items?.map((content) => (
+                <SwiperSlide key={content.name}>
                   <CarouselCategories
                     {...content}
                     queryParamName={queryParamName}
                   />
                 </SwiperSlide>
               ))}
-          </Swiper>
-          {allowTouchMove && (
-            <div className="flex justify-center items-center">
-              <div
-                className={`nextSlide${uui} bg-blue-dark-90 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center`}
-              >
-                <Icon {...iconRight} />
-              </div>
-            </div>
-          )}
-        </div>
-      )}
-      {ctaCollection?.items?.length > 0 && (
-        <div className="flex mt-6 justify-center">
-          {ctaCollection.items.map(
-            (cta) =>
-              (cta.externalLink || cta.internalLink) && (
-                <CustomLink
-                  content={cta}
-                  key={cta.name}
-                  className={classNames("button w-fit", getButtonType(view.buttonType ?? "Primario"))}
+            </Swiper>
+            {allowTouchMove && (
+              <div className="flex justify-center items-center">
+                <div
+                  className={`nextSlide${uui} bg-blue-dark-90 h-10 w-10 rounded-full cursor-pointer flex items-center justify-center`}
                 >
-                  {cta.promoTitle ?? cta.name}
-                </CustomLink>
-              )
-          )}
-        </div>
-      )}
-    </section>
-  );
-};
+                  <Icon {...iconRight} />
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        {ctaCollection?.items?.length > 0 && (
+          <div className="flex mt-6 justify-center">
+            {ctaCollection.items.map(
+              (cta) =>
+                (cta.externalLink || cta.internalLink) && (
+                  <CustomLink
+                    content={cta}
+                    key={cta.name}
+                    className={classNames("button w-fit", getButtonType(view.buttonType ?? "Primario"))}
+                  >
+                    {cta.promoTitle ?? cta.name}
+                  </CustomLink>
+                )
+            )}
+          </div>
+        )}
+      </section>
+    );
+  };
 
 export default CarouselCategoriesBlock;
