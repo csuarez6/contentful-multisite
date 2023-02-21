4ade0bba79a42f76267bdb2262488dbea26315e9
Jefferson Pérez <jperez@aplyca.com>
2023-02-21 14:52:48 -0500

    fix(breadcrumbs): fix styles

--- src/components/blocks/breadcrumbs-block/Breadcrumbs.tsx
+++ src/components/blocks/breadcrumbs-block/Breadcrumbs.tsx
@@ -4,35 +4,35 @@ import Icon from '@/components/atoms/icon/Icon';
 import { IPromoBlock } from '@/lib/interfaces/promo-content-cf.interface';
 
 const Breadcrumbs: React.FC<IPromoBlock> = ({ ctaCollection }) => {
-    if (ctaCollection?.items?.length <= 0) return;
-    return (
-        <section>
-            <nav>
-                <ul className='flex gap-2 flex-wrap'>
-                    {
-                        ctaCollection.items.map((el, i) => (
-                            <li key={el.name} className="flex items-center gap-2">
-                                {
-                                    i + 1 !== ctaCollection.items.length
-                                        ? (
-                                            <CustomLink content={el} className="text-blue-dark font-medium lg:text-lg" >
-                                                {el.promoTitle ?? el.name}
-                                            </CustomLink>
-                                        )
-                                        : (
-                                            <p className='text-blue-dark-8 font-medium lg:text-lg'>
-                                                {el.promoTitle ?? el.name}
-                                            </p>
-                                        )
-                                }
-                                {(i + 1 !== ctaCollection?.items?.length) && (<Icon icon='arrow-right' className="h-6 w-6" />)}
-                            </li>
-                        ))
-                    }
-                </ul>
-            </nav>
-        </section>
-    );
+  if (ctaCollection?.items?.length <= 0) return;
+  return (
+    <section className='py-6'>
+      <nav>
+        <ul className='flex gap-0.5 flex-wrap'>
+          {
+            ctaCollection.items.map((el, i) => (
+              <li key={el.name} className="flex items-center gap-0.5">
+                {
+                  i + 1 !== ctaCollection.items.length
+                    ? (
+                      <CustomLink content={el} linkClassName="flex items-center text[#466072] font-semibold hover:underline leading-none">
+                        {el.promoTitle ?? el.name}
+                      </CustomLink>
+                    )
+                    : (
+                      <p className='text-blue-dark font-semibold leading-none'>
+                        {el.promoTitle ?? el.name}
+                      </p>
+                    )
+                }
+                {(i + 1 !== ctaCollection?.items?.length) && (<Icon icon='arrow-right' className="h-6 w-6" />)}
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+    </section>
+  );
 };
 
 export default Breadcrumbs;
