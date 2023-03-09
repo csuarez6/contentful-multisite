47a04a55aad19746437e19644921cb37d776d79f
Andres Garcia <agarcia@aplyca.com>
2023-03-09 19:37:09 +0000

    fix(siteMap): implement sitemap.xml
    Site map

--- src/lib/services/render-blocks.service.ts
+++ src/lib/services/render-blocks.service.ts
@@ -43,7 +43,7 @@ const jsonToReactComponents = (jsonItems, attachProps = {}) => {
       isLast: key == jsonItems.length - 1,
       key: item.__typename + "-" + key,
       asBlock: true,
-      sysId: item.sys.id,
+      sysId: item.sys?.id,
     };
 
     try {
