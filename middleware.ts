e1919c97b7a72290612121cdc4e0c15a648b9606
Duvan Navarro <dnavarro@aplyca.com>
2023-05-09 17:02:53 +0000

    fix(fix-redirectPages): add redirect
    fix(fix-redirectPages): add redirect

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { getToken } from "next-auth/jwt";
+// export { default } from "next-auth/middleware";
 // paths that require authentication or authorization
-// const requireAuth: string[] = ["/registro"];
+// const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
+const checkAuth: string[] = ["/acceso", "/forgotpassword", "/registro"];
 export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: 'secret',
   });
   console.info("********** middleware: ", token);
+  const pathname = request.nextUrl.pathname;
   if (token) {
     // validate your session here
     console.info("********** Middleware Response: User Session Actived");
+    if (checkAuth.some((path) => pathname.startsWith(path))) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/`;
+      return NextResponse.rewrite(url);
+    }
+
     return NextResponse.next();
   } else {
     // the user is not logged in, redirect to the sign-in page
@@ -20,18 +29,13 @@ export async function middleware(request: NextRequest) {
     const url = request.nextUrl.clone();
     url.pathname = `/acceso`;
     url.search = `p=${requestedPage}`;
-    return NextResponse.redirect(url);
+    if (!checkAuth.some((path) => pathname.startsWith(path))) {
+      return NextResponse.redirect(url);
+    }
   }
-  // if (requireAuth.some((path) => pathname.startsWith(path))) {
-  //   //check role
-  //   if (token.role !== "admin") {
-  //     const url = new URL(`/403`, request.url);
-  //     return NextResponse.rewrite(url);
-  //   }
-  // }
 }
 
 // // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*']
+  matcher: ['/dashboard/:path*', '/acceso', '/forgotpassword', '/registro']
 };
