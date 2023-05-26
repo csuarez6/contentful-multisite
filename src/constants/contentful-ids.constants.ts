ce00906947ad600be26cbe16521e8f3adc221d83
Cesar Suarez <csuarez@aplyca.com>
2023-05-26 15:59:16 -0500

    fix: support to multiple prices in the search engine

--- src/constants/contentful-ids.constants.ts
+++ src/constants/contentful-ids.constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_FOOTER_ID = '2JxVt5rXd9IAlBlLze3MB2';
 export const DEFAULT_HEADER_ID = '1w0alkd0EpZT4mO5kr8fHr';
 export const DEFAULT_HELP_BUTTON_ID = '45oUGoePuukCo6P0feGk9r';
+export const DEFAULT_GASODOMESTICOS_PARENT_ID = 'qVeHFUsys4tPtwwgYlr93';
