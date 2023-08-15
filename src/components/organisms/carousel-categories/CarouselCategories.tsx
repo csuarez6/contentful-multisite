4f7f82f90b296a2ae27321762e85036a9ceeca89
Andres <agarcia@aplyca.com>
2023-08-15 10:40:42 -0500

    fix(contentPageBlock): implement blocks

--- src/components/organisms/carousel-categories/CarouselCategories.tsx
+++ src/components/organisms/carousel-categories/CarouselCategories.tsx
@@ -9,39 +9,37 @@ import { classNames } from "@/utils/functions";
 
 const CarouselCategories: React.FC<
   IPromoContent & ISelect & ICarouselCategoryBlock
-> = ({
-  promoTitle,
-  image,
-  name,
-  promoImage,
-  internalLink,
-  linkParameters,
-  queryParamName = "categoria",
-  value = null,
-  filterName
-}) => {
-    const { asPath } = useRouter();
-    const fullPath = asPath.split("?")[0];
+> = (props) => {
+  const {
+    promoTitle,
+    image,
+    name,
+    promoImage,
+    internalLink,
+    linkParameters,
+    queryParamName = "categoria",
+    value = null,
+    filterName,
+  } = props;
+  const { asPath } = useRouter();
+  const fullPath = asPath.split("?")[0];  
+  let link = internalLink?.urlPaths?.[0]
+    ? internalLink.urlPaths?.[0]
+    : value !== "*"
+    ? `${fullPath}?${queryParamName}=${value}`
+    : fullPath;
 
-    if (!promoImage && !promoTitle && internalLink?.urlPaths?.[0]) return;
-
-    let link = internalLink?.urlPaths?.[0]
-      ? internalLink.urlPaths?.[0]
-      : value !== "*"
-        ? `${fullPath}?${queryParamName}=${value}`
-        : fullPath;
-
-    link = linkParameters ? link + linkParameters : link;
-
-    return (
-      <article className="flex justify-center h-full">
+  link = linkParameters ? link + linkParameters : link;
+  return (
+    <article className="flex justify-center h-full">
+      {link ? (
         <Link href={link} className="h-full">
-          <div className={
-            classNames(
+          <div
+            className={classNames(
               "flex flex-col gap-2 items-center h-full group",
-              (promoImage || image) ? "justify-start" : "justify-center"
-            )
-          }>
+              promoImage || image ? "justify-start" : "justify-center"
+            )}
+          >
             {(promoImage || image) && (
               <figure className="relative w-[98px] h-[98px]">
                 <Image
@@ -49,30 +47,73 @@ const CarouselCategories: React.FC<
                   alt={promoImage?.title ?? image?.title}
                   width={promoImage?.width ?? image?.width ?? 50}
                   height={promoImage?.height ?? image?.height ?? 50}
-                  className={
-                    classNames(
-                      "object-cover rounded-full w-full h-full border border-neutral-80 group-hover:border-[3px] group-hover:border-neutral-70",
-                      (filterName !== null && (filterName?.toLowerCase() === promoTitle?.toLowerCase() || filterName?.toLowerCase() === name?.toLowerCase())) && 'border-[4px] border-neutral-70'
-                    )
-                  }
+                  className={classNames(
+                    "object-cover rounded-full w-full h-full border border-neutral-80 group-hover:border-[3px] group-hover:border-neutral-70",
+                    filterName !== null &&
+                      (filterName?.toLowerCase() ===
+                        promoTitle?.toLowerCase() ||
+                        filterName?.toLowerCase() === name?.toLowerCase()) &&
+                      "border-[4px] border-neutral-70"
+                  )}
                 />
               </figure>
             )}
             {(promoTitle || name) && (
-              <h3 className={
-                classNames(
+              <h3
+                className={classNames(
                   "text-center text-slate-600 !font-semibold group-hover:underline text-base",
-                  (filterName !== null && (filterName?.toLowerCase() === promoTitle?.toLowerCase() || filterName?.toLowerCase() === name?.toLowerCase())) && 'underline'
-                )
-              }
+                  filterName !== null &&
+                    (filterName?.toLowerCase() === promoTitle?.toLowerCase() ||
+                      filterName?.toLowerCase() === name?.toLowerCase()) &&
+                    "underline"
+                )}
               >
                 {promoTitle ?? name}
               </h3>
             )}
           </div>
         </Link>
-      </article>
-    );
-  };
+      ) : (
+        <div
+          className={classNames(
+            "flex flex-col gap-2 items-center h-full group",
+            promoImage || image ? "justify-start" : "justify-center"
+          )}
+        >
+          {(promoImage || image) && (
+            <figure className="relative w-[98px] h-[98px]">
+              <Image
+                src={promoImage?.url ?? image?.url}
+                alt={promoImage?.title ?? image?.title}
+                width={promoImage?.width ?? image?.width ?? 50}
+                height={promoImage?.height ?? image?.height ?? 50}
+                className={classNames(
+                  "object-cover rounded-full w-full h-full border border-neutral-80 group-hover:border-[3px] group-hover:border-neutral-70",
+                  filterName !== null &&
+                    (filterName?.toLowerCase() === promoTitle?.toLowerCase() ||
+                      filterName?.toLowerCase() === name?.toLowerCase()) &&
+                    "border-[4px] border-neutral-70"
+                )}
+              />
+            </figure>
+          )}
+          {(promoTitle || name) && (
+            <h3
+              className={classNames(
+                "text-center text-slate-600 !font-semibold group-hover:underline text-base",
+                filterName !== null &&
+                  (filterName?.toLowerCase() === promoTitle?.toLowerCase() ||
+                    filterName?.toLowerCase() === name?.toLowerCase()) &&
+                  "underline"
+              )}
+            >
+              {promoTitle ?? name}
+            </h3>
+          )}
+        </div>
+      )}
+    </article>
+  );
+};
 
 export default CarouselCategories;
