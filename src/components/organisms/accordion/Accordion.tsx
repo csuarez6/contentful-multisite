64947c16cc96c4d7b00a3bf8a91f58a47f83f168
jortiz19 <jortiz@aplyca.com>
2023-08-31 15:56:27 -0500

    fix(style): Fix height and markup accordion

--- src/components/organisms/accordion/Accordion.tsx
+++ src/components/organisms/accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
 import { Disclosure, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
@@ -12,112 +12,98 @@ const Accordion: React.FC<any> = ({
   columnsSize,
   displayIcon,
 }) => {
-  const [activeDisclosurePanel, setActiveDisclosurePanel] = React.useState(null);
-
-  const togglePanels = (newPanel) => {
-    if (activeDisclosurePanel) {
-      if (activeDisclosurePanel.key !== newPanel.key && activeDisclosurePanel.open) {
-        activeDisclosurePanel.close();
-      }
-    }
-    setActiveDisclosurePanel({
-      ...newPanel,
-      open: !newPanel.open
+  const $panelAccordion = useRef(null);
+  const [accordions, setAccordions] = useState([]);
+  useEffect(() => {
+    
+    const $content = $panelAccordion.current.querySelectorAll("[data-content-accordion]");
+    const $accordion = [];
+    console.log({$content })
+    $content.forEach((item, index) => {
+      item.style.height = item.firstChild.clientHeight + "px";
+      $accordion[index] = false;
     });
+    
+    window.onresize = () => {
+      $content.forEach((item, index) => {
+        item.style.height = item.firstChild.clientHeight + "px";
+        if(index == 1)
+          console.log(item.scrollHeight)
+      });
+    }
+
+    setAccordions($accordion);
+  }, []);
+
+  const onToggle = (index) => {
+    setAccordions((state) =>
+      state.map((item, i) => (i == index ? !item : false))
+    );
   };
 
   return (
-    <div className={classNames("grid", classColumns(columnsSize ?? "2"))}>
+    <div ref={$panelAccordion} className={classNames("grid", classColumns(columnsSize ?? "2"))}>
       {featuredContents?.items?.map((el, index) => {
         if (!el?.promoTitle && !el?.name) return;
+        let contentJson = el?.content?.json;
+        if (attachLinksToRichtextContent && contentJson) {
+          contentJson = attachLinksToRichtextContent(
+            contentJson,
+            el?.content?.links ?? []
+          );
+        }
         return (
-          <Disclosure
-            as="div"
-            key={`${el.name}-${index}`}
-            className="first:border-0 border-t border-neutral-80"
-          >
-            {(panel) => {
-              const { open, close } = panel;
-              let contentJson = el?.content?.json;
-              if (attachLinksToRichtextContent && contentJson) {
-                contentJson = attachLinksToRichtextContent(
-                  contentJson,
-                  el?.content?.links ?? []
-                );
-              }
-              return (
-                <>
-                  <div className="text-lg cursor-pointer">
-                    <Disclosure.Button
-                      as="div"
-                      role="button"
-                      className={`flex w-full items-center justify-between px-7 text-left text-gray-400 py-6`}
-                      onClick={ () => {                        
-                        if (!open) {
-                          close(); 
-                        }
-                        togglePanels({ ...panel, key: index });
-                      }}
-                    >
-                      <div className="flex items-center gap-3">
-                        {el.promoIcon && displayIcon && (
-                          <span className={`flow-root shrink-0 w-7 h-7 -my-2`}>
-                            <Icon
-                              icon={el.promoIcon}
-                              className="mx-auto w-full h-full text-blue-dark"
-                            />
-                          </span>
-                        )}
-                        <h3 className="text-blue-dark text-size-subtitle1 !mb-0">
-                          {el.promoTitle ?? el.name}
-                        </h3>
-                      </div>
-                      <span className="flex w-7 justify-center items-center h-auto">
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "-rotate-180" : "rotate-0",
-                            "flex w-full h-auto transform text-neutral-30"
-                          )}
-                          aria-hidden="true"
-                          width={10}
-                          height={5}
+          <>
+            <div
+              key={`${el.name}-${index}`}
+              className="first:border-0 border-t border-neutral-80"
+            >
+              <div className="text-lg cursor-pointer">
+                <div
+                  onClick={() => onToggle(index)}
+                  role="button"
+                  className={`flex w-full items-center justify-between px-7 text-left text-gray-400 py-6`}
+                >
+                  <div className="flex items-center gap-3 flex-grow">
+                    {el.promoIcon && displayIcon && (
+                      <span className={`flow-root shrink-0 w-7 h-7 -my-2`}>
+                        <Icon
+                          icon={el.promoIcon}
+                          className="mx-auto w-full h-full text-blue-dark"
                         />
                       </span>
-                    </Disclosure.Button>
+                    )}
+                    <h3 className="text-blue-dark text-size-subtitle1 !mb-0 w-1 flex-grow">
+                      {el.promoTitle ?? el.name}
+                    </h3>
                   </div>
-                  <Transition
-                    show={open}
-                    enter="transition-all ease-in duration-700"
-                    enterFrom="max-h-0 opacity-0"
-                    enterTo="max-h-screen opacity-100"
-                    leave="transition-all ease-out duration-500"
-                    leaveFrom="max-h-screen opacity-100"
-                    leaveTo="max-h-0 opacity-0"
-                  >
-                    <Disclosure.Panel
-                      as="div"
-                      className={`pb-6 ${el.promoIcon && displayIcon ? "px-10" : ""
-                        }`}
-                    >
-                      <div className="text-base text-gray-500 px-7 richtext-container custom-text">
-                        {documentToReactComponents(
-                          el?.promoDescription?.json,
-                          defaultFormatOptions
-                        )}
-                        {documentToReactComponents(
-                          contentJson,
-                          defaultFormatOptions
-                        )}
-                      </div>
-                    </Disclosure.Panel>
-                  </Transition>
-                </>
-              );
-            }}
-          </Disclosure>
+                  <span className="flex w-7 justify-center items-center h-auto">
+                    <ChevronDownIcon
+                      className={classNames(
+                        accordions[index] ? "-rotate-180" : "rotate-0",
+                        "flex w-full h-auto transform text-neutral-30"
+                      )}
+                      aria-hidden="true"
+                      width={10}
+                      height={5}
+                    />
+                  </span>
+                </div>
+              </div>
+              <div className={`overflow-hidden flex flex-col transition-[height] duration-500 ${!accordions[index] && "!h-0"}`} data-content-accordion>
+                <div className="text-base text-gray-500 px-7 richtext-container custom-text">
+                  {documentToReactComponents(
+                    el?.promoDescription?.json,
+                    defaultFormatOptions
+                  )}
+                  {documentToReactComponents(contentJson, defaultFormatOptions)}
+                </div>
+              </div>
+            </div>
+          </>
         );
       })}
-    </div >
+    </div>
   );
 };
 
