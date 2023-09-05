09b3563e7984af68b1a0351eb076f270e5559d95
Juan Camilo López Espitia <jlopez@aplyca.com>
2023-09-05 13:51:41 -0500

    fix(p2p): Se toma body raw

--- pages/api/p2p/payment-gateway/refund.ts
+++ pages/api/p2p/payment-gateway/refund.ts
@@ -7,13 +7,19 @@ import { getCLAdminCLient, isExternalPayment } from "@/lib/services/commerce-lay
 import { DEFAULT_ORDER_PARAMS } from "@/lib/graphql/order.gql";
 import uuid from "react-uuid";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) => {
 
   paymentGatewayValidation(req);
-  const { data, included }: IExternalPaymentGWRequest = req.body;
+  const { data, included }: IExternalPaymentGWRequest = JSON.parse(req.body.toString());
   const orderRequest = (included.find(item => item.type === "orders"));
 
   try {
