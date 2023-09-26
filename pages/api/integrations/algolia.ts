046768e0250d11da242cab68f6187dc65f334e7b
andresFelipeGarciaCaycedo <139148684+andresFelipeGarciaCaycedo@users.noreply.github.com>
2023-09-26 14:44:38 -0500

    fix(algolia): implement notification (#207)

---
+++ pages/api/integrations/algolia.ts
@@ -0,0 +1,25 @@
+import { getAlgoliaSearchIndex } from '@/lib/services/content-filter.service';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) => {
+    if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
+
+    try {
+        const indexSearch = getAlgoliaSearchIndex(
+            process.env.ALGOLIASEARCH_APP_ID,
+            process.env.ALGOLIASEARCH_READ_API_KEY
+        ); 
+        
+        if(!indexSearch){
+            return res.status(200).json({status: 500, data: 'commerceLayer offline'});
+        }
+        return res.status(200).json({ status: 200, data: 'commerceLayer online '});
+    } catch (e) {
+        return res.status(500).json({ status: 'error commerceLayer offline', message: e.message });
+    }
+};
+
+export default handler;
