6b1cceca7c2c972b58b9e75ec96d31a4c622db8c
andresFelipeGarciaCaycedo <139148684+andresFelipeGarciaCaycedo@users.noreply.github.com>
2023-09-26 12:48:53 -0500

    fix(place-to-pay): implement validation service (#204)

--- pages/api/integrations/place-to-pay.ts
+++ pages/api/integrations/place-to-pay.ts
@@ -1,17 +1,65 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-) => {
-    if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
-
-    try {
-       
-        return res.status(200).json({ status: 200, data: 'esta funcionando'});
-    } catch (e) {
-        return res.status(500).json({ status: 'error', message: e.message });
-    }
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  if (req.method !== "GET")
+    return res.status(405).json({ error: "Method not allowed" });
+  try {
+    placeToPay()
+      .then((result) => {
+        console.info("resuesta correcta", result);
+        return res.status(200).json({ status: 200, data: "esta funcionando" });
+      })
+      .catch((error) => {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ status: "error", message: error.message });
+      });
+  } catch (e) {
+    return res.status(500).json({ status: "error", message: e.message });
+  }
 };
+
+function placeToPay() {
+  const urlPlaceToPay = process.env.PLACE_TO_PAY_ENDPOINT + "/api/session";
+  // 'https://placetopay.com/redirection/api/session'; // URL de producción
+
+  const postData = JSON.stringify({
+    auth: {
+      login: "test",
+      tranKey: "test",
+      nonce: "test",
+      seed: new Date().toISOString(),
+    },
+  });
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Content-Length": postData.length.toString(),
+  };
+
+  return fetch(urlPlaceToPay, {
+    method: "POST",
+    headers: headers,
+    body: postData,
+  })
+    .then((response) => {
+      const statusCode = response.status;
+      if (statusCode === 401) {
+        return response.json().then(() => ({
+          isValid: "Place to pay service online",
+          statusCode: statusCode,
+        }));
+      } else {
+        return {
+          isValid: "Service offline",
+          statusCode: statusCode,
+        };
+      }
+    })
+    .catch((error) => {
+      return { error: error.message, statusCode: 500 };
+    });
+}
 
 export default handler;
