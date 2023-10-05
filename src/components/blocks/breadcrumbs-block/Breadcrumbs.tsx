8f322d7f5f8f7062ea042c67a17bc9e9bb33913d
jortiz19 <120753671+jortiz19@users.noreply.github.com>
2023-10-05 16:39:50 -0500

    fix: set the breadcrumbs to one line (#237)

--- src/components/blocks/breadcrumbs-block/Breadcrumbs.tsx
+++ src/components/blocks/breadcrumbs-block/Breadcrumbs.tsx
@@ -2,20 +2,32 @@ import React from "react";
 import CustomLink from "@/components/atoms/custom-link/CustomLink";
 import Icon from "@/components/atoms/icon/Icon";
 import { IPromoBlock } from "@/lib/interfaces/promo-content-cf.interface";
+import { Swiper, SwiperSlide, SwiperProps } from "swiper/react";
+import { FreeMode, Mousewheel } from "swiper";
+import "swiper/css/mousewheel";
+import "swiper/css/free-mode";
 
 const Breadcrumbs: React.FC<IPromoBlock> = ({ ctaCollection }) => {
+  const free:SwiperProps = {
+    modules: [FreeMode, Mousewheel],
+    slidesPerView: "auto",
+    spaceBetween: 2,
+    mousewheel: {
+      sensitivity: 1
+    },
+    freeMode: {
+      enabled: true
+    }
+  };
   return (
     <section className="py-6">
-      <nav>
-        <ul className="flex gap-0.5 flex-wrap">
-          {ctaCollection?.items.map((el, i) => (
-            <li
-              key={`${el.name ?? el.promoTitle}-${i}`}
-              className="items-center gap-0.5 hidden [&:nth-last-child(n+2):nth-last-child(-n+3)]:flex md:flex"
+      <Swiper {...free} tag="nav" wrapperTag="ul" className="text-sm md:text-base">
+        {ctaCollection?.items.map((el, i) => (
+          <SwiperSlide tag="li" key={`${el.name ?? el.promoTitle}-${i}`} className="my-auto !w-auto hidden [&:nth-last-child(n+2):nth-last-child(-n+3)]:block md:block">
+            <span
+              className="items-center gap-0.5 flex"
             >
-              {Boolean(i) && (
-                <Icon icon="arrow-right" className="h-6 w-6" />
-              )}
+              {Boolean(i) && <Icon icon="arrow-right" className="h-5 w-5 md:h-6 md:w-6" />}
               {i + 1 !== ctaCollection.items.length ? (
                 <CustomLink
                   content={el}
@@ -28,10 +40,10 @@ const Breadcrumbs: React.FC<IPromoBlock> = ({ ctaCollection }) => {
                   {el.promoTitle ?? el.name}
                 </p>
               )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+            </span>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
