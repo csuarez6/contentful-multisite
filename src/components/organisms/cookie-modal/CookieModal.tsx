225722862d8eac4785036190847c1e7522c41f33
mcermeno777 <132713422+mcermeno777@users.noreply.github.com>
2023-11-17 16:47:06 -0500

    fix(cookie-modal): adjusting modal fixed (#357)

--- src/components/organisms/cookie-modal/CookieModal.tsx
+++ src/components/organisms/cookie-modal/CookieModal.tsx
@@ -11,7 +11,7 @@ interface ICookieModal extends INavigation {
 const CookieModal: React.FC<ICookieModal> = ({
   promoTitle,
   onClose = () => null,
-  mainText
+  mainText,
 }) => {
   const [testCookie, setTestCookie] = useState("noCookie");
   const setCookies = () => {
@@ -32,44 +32,40 @@ const CookieModal: React.FC<ICookieModal> = ({
   return (
     <div
       className={classNames(
-        "fixed inset-0 z-[60]",
+        "w-full flex justify-center items-center p-2 sm:p-4 max-w-[500px] z-50 bottom-0 fixed right-0",
         testCookie ? "hidden" : "block"
       )}
     >
-      <div className={classNames("flex items-end h-full w-full justify-end")}>
-        <div className="w-full flex justify-center items-center p-2 sm:p-4 max-w-[500px]">
-          <div
-            className={classNames(
-              "relative transform w-full rounded-lg bg-white align-middle shadow-xl transition-all flex flex-col gap-12 justify-end"
-            )}
-          >
-            <div>
-              {(promoTitle || mainText ) && (
-                <div className="flex flex-col">
-                  {promoTitle && (
-                    <div className="bg-blue-dark px-6 py-4 rounded-t-lg">
-                      <h3 className="!text-white !m-0">
-                        {promoTitle ?? "Política de Cookies"}
-                      </h3>
-                    </div>
-                  )}
-                  {mainText && (
-                    <div className="text-grey-30 text-size-p3 md:text-size-p1 pt-3 pr-2 pl-5 richtext-container">
-                      {documentToReactComponents(mainText?.json)}
-                    </div>
-                  )}
-                  <div className="w-full pr-2 pb-4 pl-5 flex justify-end">
-                    <button
-                      className="button button-outline shadow-xl"
-                      onClick={handleClose}
-                    >
-                      Aceptar
-                    </button>
-                  </div>
+      <div
+        className={classNames(
+          "relative transform w-full rounded-lg bg-white align-middle shadow-xl transition-all flex flex-col gap-12 justify-end"
+        )}
+      >
+        <div>
+          {(promoTitle || mainText) && (
+            <div className="flex flex-col">
+              {promoTitle && (
+                <div className="bg-blue-dark px-6 py-4 rounded-t-lg">
+                  <h3 className="!text-white !m-0">
+                    {promoTitle ?? "Política de Cookies"}
+                  </h3>
                 </div>
               )}
+              {mainText && (
+                <div className="text-grey-30 text-size-p3 md:text-size-p1 pt-3 pr-2 pl-5 richtext-container">
+                  {documentToReactComponents(mainText?.json)}
+                </div>
+              )}
+              <div className="w-full pr-2 pb-4 pl-5 flex justify-end">
+                <button
+                  className="button button-outline shadow-xl"
+                  onClick={handleClose}
+                >
+                  Aceptar
+                </button>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
