479f75a63c91ffa53afa14bccc88052e86d1e9c8
Cesar Suarez <csuarez@aplyca.com>
2024-02-29 16:29:21 -0500

    fix: update function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-export const setUpMultisite = (request, config) => {
+import { serverRuntimeConfig } from "next.config.js";
+
+export const setUpMultisite = (request) => {
   try {
+    const config = serverRuntimeConfig?.siteList;
     const domain = request?.headers?.get("host");
     const path = request?.nextUrl?.pathname;
 
+    console.log("The domain is:", domain);
+    console.log("The path is:", path);
+    
     const defaultSite = {
       is_default_site: true,
       domain: "localhost",
       root_path: "/",
-      site_paths: "^/((?!api|_next/static|_next/image|favicon.ico).*)",
+      site_paths: ".*"
     };
 
-    const defaultSiteList = [defaultSite];
-    const siteList = config?.siteList ?? defaultSiteList;
+    const siteList = config ?? [defaultSite];
 
     for (const site of siteList) {
       if (
-        domain?.match(site?.domain) !== null ||
+        domain.match(site?.domain) !== null ||
         site?.is_default_site === true
       ) {
         if (path.match(site?.site_paths ?? defaultSite?.site_paths)) {
-          const newUrl = `${site?.root_path}${path}`;
-          return new URL(newUrl, request?.url);
+          const newUrl = new URL(`${site?.root_path}${path}`, request?.url);
+          console.log("The newUrl is:", newUrl);
+          return NextResponse.rewrite(newUrl);
         }
-        break;
       }
     }
   } catch (error) {
